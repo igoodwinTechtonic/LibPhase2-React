616f5e5c807ef52c829da3706fec387ae2fc24e6
igoodwinTechtonic <ian.goodwin@techtonic.com>
2020-12-09 16:32:50 -0600

    Backup before moving to Library project repo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,18 @@
 import * as React from "react";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
   return (
     <ul className="menu__box" id="ham-nav">
-      <Link className="menu__item" to=""><li className="header__link-text">Home</li></Link>
-      <Link className="menu__item" to="bookshelf"><li className="header__link-text">Bookshelf</li></Link>
-      <Link className="menu__item" to="add"><li className="header__link-text">Add Book</li></Link>
+      <Link className="menu__item" to="/">
+        <li className="header__link-text">Home</li>
+      </Link>
+      <Link className="menu__item" to="/bookshelf">
+        <li className="header__link-text">Bookshelf</li>
+      </Link>
+      <Link className="menu__item" to="/add">
+        <li className="header__link-text">Add Book</li>
+      </Link>
     </ul>
   );
 };
